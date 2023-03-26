f47a273db33a38910650bd4286557ca27b830544
Julian Worden <93101937+julianworden@users.noreply.github.com>
2023-03-26 12:58:00 -0700

    Initial Commit

---
+++ Accountable/AccountableApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccountableApp.swift
+//  Accountable
+//
+//  Created by Julian Worden on 3/26/23.
+//
+
+import SwiftUI
+
+@main
+struct AccountableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Accountable/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Accountable
+//
+//  Created by Julian Worden on 3/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
